30fc64f91d0236e31c9a6e4144c804b09bc2bde9
Younes Darabi <younes.darabi@outlook.com>
2025-10-22 18:28:29 +0200

    Add why me

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { Menu } from './shared/menu/menu';
 import { SocialMedia } from './shared/social-media/social-media';
 import { Hero } from './hero/hero';
+import { WhyMe } from './why-me/why-me';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Menu,SocialMedia,Hero],
+  imports: [RouterOutlet, Menu, SocialMedia, Hero, WhyMe],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  
+
 }
