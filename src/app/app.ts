38c9ea6113b4a9ba797336b53df5fe338a4e2023
Younes Darabi <younes.darabi@outlook.com>
2025-10-22 13:12:52 +0200

    Add menu

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Menu } from './shared/menu/menu';
+import { SocialMedia } from './shared/social-media/social-media';
+import { Hero } from './hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Menu,SocialMedia,Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('portfolio');
+  
 }
